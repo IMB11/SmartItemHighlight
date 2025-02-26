c9565916412e9062d8c47ef3a2c608a38859aa1a
Calum <hendersoncal117@gmail.com>
2025-02-26 12:37:05 +0000

    Dynamic rendering

--- src/main/java/dev/imb11/smartitemhighlight/SmartItemHighlight.java
+++ src/main/java/dev/imb11/smartitemhighlight/SmartItemHighlight.java
@@ -2,10 +2,11 @@ package dev.imb11.smartitemhighlight;
 
 import dev.imb11.mru.LoaderUtils;
 import dev.imb11.smartitemhighlight.api.SIHEvent;
+import dev.imb11.smartitemhighlight.api.condition.HighlightCondition;
 import dev.imb11.smartitemhighlight.api.condition.RenderFunction;
 import dev.imb11.smartitemhighlight.api.events.ItemHighlightEvents;
 import dev.imb11.smartitemhighlight.config.HighlightConditionManager;
-import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.renderer.RenderType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,13 +26,25 @@ public class SmartItemHighlight {
     public static final Map<ResourceLocation, RenderFunction> RENDER_FUNCTION_REGISTRY = new HashMap<>();
 
     public static void initialize() {
-        ItemHighlightEvents.SHOULD_RENDER.register(((drawContext, livingEntity, world, stack, x, y, seed, z) -> highlightedItems.contains(stack.getItem().getDescriptionId()) ? SIHEvent.CallbackResult.SUCCESS : SIHEvent.CallbackResult.CONTINUE));
+        ItemHighlightEvents.SHOULD_RENDER.register(((drawContext, livingEntity, world, stack, x, y, seed, z) -> {
+            // Do we really need this event anymore now that its being handled by the HighlightCondition class?
+            return SIHEvent.CallbackResult.SUCCESS;
+        }));
+
         ItemHighlightEvents.RENDER_HIGHLIGHT.register(((drawContext, livingEntity, world, stack, x, y, seed, z) -> {
-            RENDER_FUNCTION_REGISTRY.get(ResourceLocation.fromNamespaceAndPath(MOD_ID, "default")).render(drawContext, x, y);
+            for (HighlightCondition loadedCondition : HighlightConditionManager.getLoadedConditions()) {
+               if (loadedCondition.isEnabled())
+                   if (loadedCondition.shouldHighlightStack((ClientLevel) world, stack)) {
+                       ResourceLocation renderFunction = loadedCondition.getRenderFunction();
+                       RenderFunction function = RENDER_FUNCTION_REGISTRY.get(renderFunction);
+                       if (function != null)
+                           function.render(loadedCondition, world, livingEntity, stack, seed, drawContext, x, y, z);
+                   }
+            }
         }));
 
         // Register default render functions
-        RENDER_FUNCTION_REGISTRY.put(ResourceLocation.fromNamespaceAndPath(MOD_ID, "default"), (drawContext, x, y) -> {
+        RENDER_FUNCTION_REGISTRY.put(ResourceLocation.fromNamespaceAndPath(MOD_ID, "default"), (condition, world, livingEntity, stack, seed, drawContext, x, y, z) -> {
             drawContext.fill(RenderType.gui(), x, y, x + 16, y + 16, Integer.MAX_VALUE);
             Utils.renderTexture(drawContext, "textures/star.png", x, y, 16, 16);
         });
--- src/main/java/dev/imb11/smartitemhighlight/api/condition/RenderFunction.java
+++ src/main/java/dev/imb11/smartitemhighlight/api/condition/RenderFunction.java
@@ -1,8 +1,11 @@
 package dev.imb11.smartitemhighlight.api.condition;
 
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 
 @FunctionalInterface
     public interface RenderFunction {
-        void render(GuiGraphics drawContext, int x, int y);
+        void render(HighlightCondition condition, Level world, LivingEntity livingEntity, ItemStack stack, int seed, GuiGraphics drawContext, int x, int y, int z);
     }
